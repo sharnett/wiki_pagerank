1dcf48516a0dd5c47a2b1f60468a945a67554b93
Sean Harnett <srharnett@gmail.com>
2013-12-28 16:47:13 -0800

    removed unused files and updated gitignore

--- create_compact_dicts.py
+++
@@ -1,9 +0,0 @@
-from cPickle import dump
-sparse_to_dense = {}
-dense_to_sparse = []
-for i, line in enumerate(open('graph.txt')):
-    ID = int(line.split()[0])
-    sparse_to_dense[ID] = i
-    dense_to_sparse.append(ID)
-dump(sparse_to_dense, open('sparse_to_dense.pickle', 'w'), 2)
-dump(dense_to_sparse, open('dense_to_sparse.pickle', 'w'), 2)
